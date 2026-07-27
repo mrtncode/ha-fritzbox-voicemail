eb093c287a18c21a4b6ca343e292f8d057cff9c0
M <145050460+mrtncode@users.noreply.github.com>
2026-07-27 15:15:00 +0000

    Fix lint

--- custom_components/fritzbox_voicemail/media_source.py
+++ custom_components/fritzbox_voicemail/media_source.py
@@ -27,9 +27,7 @@ class MailboxMediaSource(media_source.MediaSource):
         self.hass = hass
         self.name = "Mailbox"
 
-    async def async_browse_media(
-        self, item: media_source.MediaSourceItem | None = None
-    ) -> media_source.BrowseMediaSource:
+    async def async_browse_media(self) -> media_source.BrowseMediaSource:
         """Browse media items."""
         runtime_data = next(iter(self.hass.data[DOMAIN].values()))
         fritz_connection = runtime_data.client
